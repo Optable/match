a5ac1a90b954614a8015950d1fe4d790b6ea91b7
Justin <justin.li@optable.co>
2021-08-27 16:43:24 -0400

    lower back oprf test number

--- internal/oprf/oprf_test.go
+++ internal/oprf/oprf_test.go
@@ -14,7 +14,7 @@ import (
 var (
 	network   = "tcp"
 	address   = "127.0.0.1:"
-	baseCount = 14000000
+	baseCount = 10000
 	prng      = rand.New(rand.NewSource(time.Now().UnixNano()))
 	choices   = genChoiceString()
 )
