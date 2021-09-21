a7c39eb3051aab09587a33ea21a077a2d5345ac4
Justin <justin.li@optable.co>
2021-09-21 09:05:54 -0400

    lower number of oprf for tests

--- internal/oprf/oprf_test.go
+++ internal/oprf/oprf_test.go
@@ -14,7 +14,7 @@ import (
 var (
 	network   = "tcp"
 	address   = "127.0.0.1:"
-	baseCount = 1000000
+	baseCount = 1024
 	prng      = rand.New(rand.NewSource(time.Now().UnixNano()))
 	choices   = genChoiceString()
 	k         = 512
