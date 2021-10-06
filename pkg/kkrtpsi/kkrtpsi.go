2bbcfe81504541f69c221a1bd11765ea7d29b26a
Justin <justin.li@optable.co>
2021-10-06 17:20:35 -0400

    remove findK since no longer needed

--- pkg/kkrtpsi/kkrtpsi.go
+++ pkg/kkrtpsi/kkrtpsi.go
@@ -15,29 +15,6 @@ const (
 	batchSize = 2048
 )
 
-// findK returns the number of base OT for OPRF
-// these numbers are from the paper: Efficient Batched Oblivious PRF with Applications to Private Set Intersection
-// by Vladimir Kolesnikov, Ranjit Kumaresan, Mike Rosulek, and Ni Treu in 2016.
-// Reference:	http://dx.doi.org/10.1145/2976749.2978381 (KKRT)
-func findK(n int64) int {
-	switch {
-	// 2^8
-	case n > 0 && n <= 256:
-		return 424
-	// 2^12
-	case n > 256 && n <= 4096:
-		return 432
-	// 2^16
-	case n > 4096 && n <= 65536:
-		return 440
-	// 2^20
-	case n > 65536:
-		return 448
-	default:
-		return 128
-	}
-}
-
 // HashRead reads one hash
 func EncodesRead(r io.Reader, u *[cuckoo.Nhash]uint64) (err error) {
 	err = binary.Read(r, binary.BigEndian, u)
