3cc90e1b81e0f3880e10fc3847172155a4badfba
Xavier Capaldi <xavier.capaldi@optable.co>
2021-11-30 16:34:17 -0500

    Simplify concurrency in encodeAndHash
    Remove complex job structure and replicate goroutine model used
    in the rest of the library for transpose and bit operations.

--- pkg/kkrtpsi/kkrtpsi.go
+++ pkg/kkrtpsi/kkrtpsi.go
@@ -14,8 +14,6 @@ import (
 	"github.com/optable/match/internal/oprf"
 )
 
-const batchSize = 2048
-
 // HashRead reads one hash
 func EncodesRead(r io.Reader, u *[cuckoo.Nhash]uint64) (err error) {
 	err = binary.Read(r, binary.BigEndian, u)
@@ -25,20 +23,6 @@ func EncodesRead(r io.Reader, u *[cuckoo.Nhash]uint64) (err error) {
 // HashWrite writes one hash out
 func EncodesWrite(w io.Writer, u [cuckoo.Nhash]uint64) error {
 	return binary.Write(w, binary.BigEndian, u)
-}
-
-type hashEncodingJob struct {
-	batchSize int
-	prc       []oprfEncodedInputs // PseudoRandom Code
-	hashed    [][cuckoo.Nhash]uint64
-	execute   func(job hashEncodingJob)
-}
-
-func makeJob(hasher hash.Hasher, batchSize int, f func(hashEncodingJob)) hashEncodingJob {
-	return hashEncodingJob{
-		batchSize: batchSize,
-		prc:       make([]oprfEncodedInputs, batchSize),
-		execute:   f}
 }
 
 func (bytes oprfEncodedInputs) encodeAndHash(oprfKeys *oprf.Key, hasher hash.Hasher) (hashes [cuckoo.Nhash]uint64, err error) {
@@ -54,68 +38,44 @@ func (bytes oprfEncodedInputs) encodeAndHash(oprfKeys *oprf.Key, hasher hash.Has
 	return hashes, nil
 }
 
-// HashAllParallel reads all identifiers from identifiers
-// and parallel hashes them until identifiers closes
+// HashAllParallel accepts the inputsAndHasher struct
+// which contains the identifiers and the hasher and
+// parallel hashes them until identifiers closes
 func EncodeAndHashAllParallel(oprfKeys *oprf.Key, message inputsAndHasher) <-chan [cuckoo.Nhash]uint64 {
-	// one wg.Add() per batch + one for the batcher go routine
-	var jobPool = make(chan hashEncodingJob)
-	var wg sync.WaitGroup
+	nworkers := runtime.GOMAXPROCS(0)
+	var encoded = make(chan [cuckoo.Nhash]uint64, 1024)
 
-	// work on the jobPool
-	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
+	// determine number of blocks to split original matrix
+	workerResp := len(message.inputs) / nworkers
+
+	// Run a worker pool
+	var wg sync.WaitGroup
+	wg.Add(nworkers)
+	for w := 0; w < nworkers; w++ {
+		w := w
 		go func() {
-			var err error
-			for job := range jobPool {
-				var hashed = make([][cuckoo.Nhash]uint64, job.batchSize)
-				for i := 0; i < job.batchSize; i++ {
-					hashed[i], err = job.prc[i].encodeAndHash(oprfKeys, message.hasher)
+			defer wg.Done()
+			step := workerResp * w
+			if w == nworkers-1 { // last block
+				for i := step; i < len(message.inputs); i++ {
+					hashes, err := message.inputs[i].encodeAndHash(oprfKeys, message.hasher)
 					if err != nil {
 						panic(err)
 					}
+					encoded <- hashes
 				}
-				job.hashed = hashed
-				job.execute(job)
+			} else {
+				for i := step; i < step+workerResp; i++ {
+					hashes, err := message.inputs[i].encodeAndHash(oprfKeys, message.hasher)
+					if err != nil {
+						panic(err)
+					}
+					encoded <- hashes
+				}
 			}
 		}()
 	}
 
-	var encoded = make(chan [cuckoo.Nhash]uint64)
-	execute := func(job hashEncodingJob) {
-		// pump everything out
-		for i := 0; i < job.batchSize; i++ {
-			encoded <- job.hashed[i]
-		}
-		wg.Done()
-	}
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		var i = 0
-		// init a first batch
-		var batch = makeJob(message.hasher, batchSize, execute)
-		for _, identifier := range message.inputs {
-			// accumulate a batch
-			batch.prc[i] = identifier
-			i++
-			// send it out?
-			if i == batchSize {
-				wg.Add(1)
-				jobPool <- batch
-				// reset batch
-				batch = makeJob(message.hasher, batchSize, execute)
-				i = 0
-			}
-		}
-		// anything left here?
-		if i != 0 {
-			batch.batchSize = i
-			wg.Add(1)
-			jobPool <- batch
-		}
-	}()
-
-	// turn the lights off on your way out
 	go func() {
 		wg.Wait()
 		close(encoded)
